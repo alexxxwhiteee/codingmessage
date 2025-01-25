db49211e247c1c3dc3ae6bf054ba9a02d2062a72
Aleksandr Vasiliev <alexvasiliev93@mail.ru>
2025-01-25 21:42:46 +0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? '/codingmessage/' : '/'
+
+  return {
+    base,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
